6891aa4e8d580c837b6a5e4d4e80bd6423dbd2cd
Nathan Lucas <b.n.lucas@plainwreck.com>
2012-11-14 02:41:21 -0600

    Update TimeStamp/Timestamp.py

--- TimeStamp/Timestamp.py
+++ TimeStamp/Timestamp.py
@@ -5,9 +5,7 @@ class TimestampCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
 
-		timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
 		sels = self.view.sel()
 		for sel in sels:
 			self.view.erase(edit, sel)
-			self.view.insert(edit, sel.begin(), timestamp)
+			self.view.insert(edit, sel.begin(), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
