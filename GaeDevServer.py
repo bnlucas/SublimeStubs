5d1a7262b80e905ae2ce201b5130e51a80f237c2
Nathan Lucas <nathan@bnlucas.com>
2013-05-30 05:47:51 -0500

    Update GaeDevServer.py

--- GaeDevServer.py
+++ GaeDevServer.py
@@ -5,17 +5,17 @@ import sublime, sublime_plugin
 
 import os
 
-path = '/path/to/google_app_engine_sdk'
+from threading import Thread
+
+path = 'C:\\nathan\\appengine'
 
 class GaeDevServerCommand(sublime_plugin.WindowCommand):
 
-  def run(self, paths=[]):
+	def run(self, paths=[]):
 
 		if os.path.dirname(paths[0]) == path:
-			app = paths[0].replace(''.join([path, os.sep]), '')
-
+			app    = paths[0].replace(''.join([path, os.sep]), '')
 			server = 'python dev_appserver.py --clear_datastore=yes {app}/'
+			cmd    = 'cd {path} && {server}'.format(path=path, server=server)
 
-			cmd = 'cd {path} && {server}'.format(path=path, server=server)
-
-			os.system(cmd.format(app=app))
+			Thread(target=os.system, args=[cmd.format(app=app)]).start()
